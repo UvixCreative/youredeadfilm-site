9af219031b8bfd6b5a0dd956a73fe9c0c3de08c2
Ben Rankin <me@benrank.in>
2026-02-13 04:45:27 -0500

    Added monbile breakpoint, embedded Courier font from Google, removed _site artifact from git repo

--- _config.ts
+++ _config.ts
@@ -1,6 +1,6 @@
 import lume from "lume/mod.ts";
 import blog from "blog/mod.ts";
-import * as fs from "node:fs";
+import googleFonts from "lume/plugins/google_fonts.ts";
 // import transformImages from "lume/plugins/transform_images.ts";
 
 const site = lume({
@@ -9,6 +9,10 @@ const site = lume({
 
 // site.use(transformImages());
 site.use(blog());
+site.use(googleFonts({
+  fonts:
+    "https://fonts.googleapis.com/css2?family=Courier+Prime:ital,wght@0,400;0,700;1,400;1,700&display=swap",
+}));
 
 site.add("css");
 site.add("assets");
